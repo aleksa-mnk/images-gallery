cee06af64378353d320d3e15fd9f159d46327449
unknown <alyonamankevich@gmail.com>
2020-02-20 15:23:44 +0300

    change 1

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-    $dir = 'img';
+    $dir = 'images';
     $img = scandir ($dir);
     echo '<table border=1>';
     for($i=2; $i<7; $i++){
-        
-        echo '<tr>
-            <img src="img/'.$img[$i].'><br> 
-            </tr>'
+        echo '<tr><td>
+            <img src="images/'.$img[$i].'"><br> 
+        </td></tr>  '
         ;            
     }           
     echo '</table>';
